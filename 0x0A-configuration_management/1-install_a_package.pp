5c706b43785dd7ce4380cf3dfe8388e40cf89e2a
Devix-old <mohamed.elgharef@gmail.com>
2023-11-25 05:06:35 +0100

    sss

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,16 @@
-# install Flask
-package { 'Flask':
+#!/usr/bin/pup
+# install flask
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
