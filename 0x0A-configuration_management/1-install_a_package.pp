c885bf5747a604c41a56582c9b72fd1196bd5b4a
Devix-old <mohamed.elgharef@gmail.com>
2023-11-25 03:54:01 +0100

    sss

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+# install Flask
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,4 @@
+#Kill killmenow
+exec{'pkill -f killmenow':
+path  => '/usr/bin/:/usr/local/bin/:/bin/'
+}
