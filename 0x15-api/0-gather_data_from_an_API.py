161c06942174d0a4c3a6c449c00bf5ff6ae5c014
Devix-old <mohamed.elgharef@gmail.com>
2024-01-29 23:41:08 +0100

    git add .!

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Fetch user and task data from JSONPlaceholder API and print completed task info
+Usage: python script_name.py <user_id>
+"""
+
+import urllib.request
+import json
+import sys
+
+def get_user_name(user_data, user_id):
+    """Get user name based on ID."""
+    for user in user_data:
+        if user["id"] == user_id:
+            return user["name"]
+    return None
+
+def get_tasks(todo_data, user_id):
+    """Get completed task information for a user."""
+    tasks = {"completed": 0, "total_tasks": 0}
+    for todo in todo_data:
+        if todo["userId"] == user_id:
+            tasks["total_tasks"] += 1
+            if todo["completed"]:
+                tasks["completed"] += 1
+    return tasks
+
+def print_completed_tasks(todo_data, user_id):
+    """Print titles of completed tasks for a user."""
+    for todo in todo_data:
+        if todo["userId"] == user_id and todo["completed"]:
+            print(" \t" + todo["title"])
+
+if __name__ == "__main__":
+    user_id = int(sys.argv[1])
+
+    # Fetch data from the API
+    todos_data = urllib.request.urlopen(
+        "https://jsonplaceholder.typicode.com/todos").read()
+    users_data = urllib.request.urlopen(
+        "https://jsonplaceholder.typicode.com/users").read()
+
+    # Parse JSON data
+    todos = json.loads(todos_data.decode("utf-8"))
+    users = json.loads(users_data.decode("utf-8"))
+
+    # Get user name
+    user_name = get_user_name(users, user_id)
+
+    if user_name is not None:
+        # Get tasks information
+        tasks_info = get_tasks(todos, user_id)
+        completed_tasks = tasks_info["completed"]
+        total_tasks = tasks_info["total_tasks"]
+
+        # Print the result
+        print(f"Employee {user_name} is done \
+              with tasks ({completed_tasks}/{total_tasks}):")
+        print_completed_tasks(todos, user_id)
