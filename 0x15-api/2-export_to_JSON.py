c5dc0e99f334e37ac5e9c2a704563ab4f53a900e
Devix-old <mohamed.elgharef@gmail.com>
2024-01-30 03:30:42 +0100

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: {} USER_ID".format(argv[0]))
+    else:
+        user_id = argv[1]
+
+        # Fetch user data
+        user_response = requests.get(
+            f'https://jsonplaceholder.typicode.com/users/{user_id}')
+        user_data = user_response.json()
+
+        # Fetch tasks data for the given user ID
+        tasks_response = requests.get(
+            f'https://jsonplaceholder.typicode.com/todos?userId={user_id}')
+        tasks_data = tasks_response.json()
+
+        # Prepare the JSON structure
+        json_data = {
+            user_id: [
+                {
+                    "task": task["title"],
+                    "completed": task["completed"],
+                    "username": user_data["username"]
+                } for task in tasks_data
+            ]
+        }
+
+        # Write to JSON file
+        with open(f'{user_id}.json', 'w') as json_file:
+            json.dump(json_data, json_file)
